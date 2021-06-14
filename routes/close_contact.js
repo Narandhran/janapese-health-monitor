9f1950aaefe339ee0e719d0633b27bb179a90051
narandhran <narandhran@gmail.com>
2021-06-14 12:41:01 +0530

    uuid altered

--- routes/close_contact.js
+++ routes/close_contact.js
@@ -2,5 +2,5 @@ const { authenticate, authorize } = require("../middlewares/auth")
 const closeContactControl = require('../controllers/close_contact');
 
 module.exports = app => {
-    app.post('/close_contact/w/create', authenticate, authorize(['ADMIN']), closeContactControl.insertData);
+    app.post('/close_contact/w/create', authenticate, authorize(['ADMIN','USER']), closeContactControl.insertData);
 }
