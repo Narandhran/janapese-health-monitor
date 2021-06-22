74a9b25e479c4140be3afd89c336046fd76e65f2
narandhran <narandhran@gmail.com>
2021-06-22 08:36:05 +0530

    filter fixed

--- controllers/medical_report.js
+++ controllers/medical_report.js
@@ -144,31 +144,24 @@ module.exports = {
      * Report screen
      */
     filter: async (req, res) => {
-        let { name = '', empId = '', department = '', sDate = '', tDate = '' } = req.body;
-        let fsDate = moment(sDate).utcOffset(0), ftDate = moment(tDate).utcOffset(0);
-        fsDate.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
-        ftDate.set({ hour: 0, minute: 0, second: 0, millisecond: 0 });
+        let { name, empId, department, sDate, tDate } =  req.body;
         let filterQuery = {};
-        if (name || empId || department) {
-            filterQuery = {
-                $or: [
-                    { name }, { empId }, { department }
-                ]
-            }
+        if (!name && !empId && !department && (sDate == tDate))
+            errorHandler(req, res, new Error('Select atlease one parameter to search'));
+        else {
+            if (name) filterQuery.name = name.toLowerCase();
+            if (empId) filterQuery.empId = empId.toUpperCase();
+            if (department) filterQuery.department = department.toUpperCase();
+            if (sDate != tDate) filterQuery.createdAt = { $gt: new Date(sDate), $lte: new Date(tDate) };
+
+            await MedicalReport
+                .find(filterQuery)
+                .sort({ createdAt: -1 })
+                .exec((err, data) => {
+                    if (err) errorHandler(req, res, err);
+                    else successHandler(req, res, toJapanese['Data listed successfully'], data);
+                });
         }
-        if (sDate != tDate) {
-            if (name || empId || department)
-                filterQuery.$or.push({ date: { $gt: fsDate, $lte: ftDate } });
-            else
-                filterQuery = { date: { $gt: fsDate, $lte: ftDate } };
-        }
-        await MedicalReport
-            .find(filterQuery)
-            .sort({ createdAt: -1 })
-            .exec((err, data) => {
-                if (err) errorHandler(req, res, err);
-                else successHandler(req, res, toJapanese['Data listed successfully'], data);
-            });
     },
     getByUser: async (req, res) => {
         await MedicalReport
--- t.js
+++ t.js
@@ -1,30 +1,9 @@
-const MedicalReport = require('./models/medical_report');
 const moment = require('moment');
-const user = require('./models/user');
-const { loadFcmMessage, sendFcmMessagePromise } = require('./utils/fcm');
 
 
-let date = moment(new Date()).format("YYYY-MM-DD")
-let fn = async () => {
-    await MedicalReport.find({ createdAt: { $gt: date } }, async (err, data) => {
-        if (err) console.log(err);
-        else {
-            let uuids = data.map(e => {
-                return e.empId;
-            });
-            let senderData = await user.find({ empId: { $nin: uuids }, fcmToken: { $ne: '' } }, 'fcmToken').lean();
-            let senderIds = senderData.map(e => {
-                return e.fcmToken;
-            });
-            let bodyMessage = '本日の健康状態の登録がまだ実施されておりません。'
-                + '健康状態の入力後、登録をお願いします。'
-                + '未登録日：06月11日分 ' + moment(new Date()).format("DD/MM/YYYY")
-                + ' 従業員の皆様とそのご家族様を守る為、ご協力をお願いいたします';
-            let options = loadFcmMessage(senderIds, '通知メッセージ', bodyMessage);
-            sendFcmMessagePromise(options)
-                .then(() => {
-                    console.log('Data send successfully');
-                }).catch(e => console.log(`${e.status || 400} - ${e.message}`));
-        }
-    })
-}
+let dateString = {};
+let { name, age } = dateString
+if(!name)
+console.log(`
+    ${dateString[Object.keys(dateString)[0]]}
+    `);
